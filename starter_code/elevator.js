49473b41a394d401130f06ecf7f0a21f784c7626
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-07-03 19:51:00 +0200

    INTERATION 3

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,47 @@
+/*jshint esversion:6 */
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 10;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
-}
+  start() {
+    this.timer = setInterval(() => this.update(), 1 * 1000);
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  update() {
+    this.log();
+  }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+    this.floorDown();
 
+  }
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {
+    if (this.floor < this.MAXFLOOR){
+      this.floor++;
+      console.log("Subiendo");
+    } else {
+      this.stop();
+    }
+
+  }
+  floorDown() {
+    if (this.floor > 0){
+      this.floor--;
+      console.log("bajando");
+    }else{
+      this.stop();
+    }
+  }
+  call() {}
+
+
+}
 module.exports = Elevator;
